35d4c033aae40434b8b6061b717d7724d8a8642f
BazzaBazaa <bas.maw.writer@gmail.com>
2020-09-30 11:20:26 +0100

    working version

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.button');
+const button1 = document.querySelector('.button1');
+
+button.addEventListener("click", function(){ alert("Not real Google!"); });
+button1.addEventListener("click", function(){ alert("Not real Google!"); });
+
+//function notReal() {
+  //alert("Not real Google!");
+//}
